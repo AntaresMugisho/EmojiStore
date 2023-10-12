03b6884a0d42dace1d576150ed97efd95004c17e
AntaresMugisho <antaresmugisho@gmail.com>
2023-10-12 23:47:51 +0200

    Use saved emoji file

--- EmojiStore/main.py
+++ EmojiStore/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+from bs4 import BeautifulSoup
+import requests
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+url = "https://www.webfx.com/tools/emoji-cheat-sheet/"
+# response = requests.get(url)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def scrap():
+    with open("../emoji.html", "r") as html:
+        soup = BeautifulSoup(html, "html.parser")
+        list = soup.select("div.apple.emojicon")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        url = list[0].find("img").get("src")
+        print(url)
+
+if __name__ == "__main__":
+    scrap()
