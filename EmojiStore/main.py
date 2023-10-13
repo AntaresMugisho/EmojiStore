9d4f4cbc7726576cd6a86ffaf0e090575e60aa2d
AntaresMugisho <antaresmugisho@gmail.com>
2023-10-13 02:02:48 +0200

    Successfully getting all emoji list and details

--- EmojiStore/main.py
+++ EmojiStore/main.py
@@ -12,21 +12,34 @@ def scrap():
     with open("../emoji.html", "r") as html:
         soup = BeautifulSoup(html, "html.parser")
 
-        category = soup.select_one(f"div#{categories[0]}")
-        item = category.select_one("div._item")
-        description = item.get("data-alt-name")
+        count = 0
+        for i, category in enumerate(categories):
+            print(f"{i+1} => {category.upper()}")
+            container = soup.select_one(f"div#{category}")
+            items = container.select("div._item")
 
-        apple_emoji = item.select_one("div.apple.emojicon")
-        image = apple_emoji.select_one("img")
-        emoji = image.get("alt")
-        emojicon_url = image.get("src")
+            for j, item in enumerate(items):
+                description = item.get("data-alt-name")
 
-        shortcode = item.select_one("div.shortcode").text
-        unicode = item.select_one("div.unicode").text
+                apple_emoji = item.select_one("div.apple.emojicon")
+                image = apple_emoji.select_one("img")
 
+                try:
+                    emoji = image.get("alt")
+                except AttributeError:
+                    google_emoji = item.select_one("div.google.emojicon")
+                    image = google_emoji.select_one("img")
+                    emoji = image.get("alt")
+                    emojicon_url = image.get("src")
 
-        print(f"Category: {category.get('id')} \nDescription: {description} \nEmoji: {emoji} \nShortcode: {shortcode} \nUnicode: {unicode}")
+                shortcode = item.select_one("div.shortcode").text
+                unicode = item.select_one("div.unicode").text
+                count+= 1
 
+                print(f"\t{i+1}.{j+1}=> Category: {category} Description: {description} Emoji: {emoji} Shortcode: {shortcode} Unicode: {unicode}")
+            print(f"# ENDS WITH {j+1} EMOJIS #-----------------------------\n")
+
+        print(f"# TOTAL OF {count} EMOJIS FOUND #-----------------------------")
 
 
 if __name__ == "__main__":
